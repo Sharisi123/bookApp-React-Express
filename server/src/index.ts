99fda1106d0ad3d62b683bbffdd5e3d0dcff520d
Alex <nasir1231011@gmail.com>
2021-09-13 12:35:02 +0300

    heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
 const mongoDB = require("./mongoDB");
+const path = require("path");
 
 const booksRouter = require("./modules/books/");
 const authorsRouter = require("./modules/authors");
@@ -40,6 +41,12 @@ app.use("/api", authorsRouter.router);
 app.use("/api/users", usersRouter.router);
 
 mongoDB();
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server starts on port http://localhost:${port}`);
